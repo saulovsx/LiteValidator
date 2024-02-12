0f3609f2a5fa1a73fd49a9e1c97c25aa6ae7fbf2
Saulo Vinícius Silva <saulovsx@gmail.com>
2024-02-12 16:42:49 -0300

    feat: Add Classes de exemplo - Client e product

---
+++ LiteValidator/Client.cs
@@ -0,0 +1,37 @@
+﻿namespace LiteValidator
+{
+    public class Client(int id, string name)
+    {
+        public int Id { get; set; } = id;
+        public string? Name { get; set; } = name;
+
+        public void AddClient()
+        {
+            Console.WriteLine("Validando Client.");
+            var validatorResult = BuilderValidateClient().Validate(this);
+            
+            if (!validatorResult.IsValid)
+            {
+                foreach (var item in validatorResult.Messages)
+                {
+                    Console.WriteLine(item);
+                }
+                return;
+            }
+            SaveClient();
+        }        
+
+        LiteValidator<Client> BuilderValidateClient()
+        {
+            return new LiteValidator<Client>()
+            .AddRule(f => !string.IsNullOrEmpty(f.Name))
+            .AddError("*** Nome não pode ser nulo. ***")
+            .Build();
+        }
+        public void SaveClient()
+        {
+            //Implementação de persistência
+            Console.WriteLine("Salvando Client.");
+        }
+    }
+}
---
+++ LiteValidator/Product.cs
@@ -0,0 +1,49 @@
+﻿namespace LiteValidator
+{
+    public class Product(int id, string name, decimal price, decimal quantity)
+    {
+        public int Id { get; set; } = id;
+        public string Name { get; set; } = name;
+        public decimal Price { get; set; } = price;
+        public decimal Quantity { get; set; } = quantity;
+        public decimal Total 
+        {
+            get { return Price * Quantity; }
+        }
+
+        public void AddProduct()
+        {
+            Console.WriteLine("Validando Produto.");            
+            var validatorResult = ValidateProduct(this);
+
+            if (!validatorResult.IsValid)
+            {
+                foreach (var item in validatorResult.Messages)
+                {
+                    Console.WriteLine(item);
+                }
+                return;
+            }
+            SaveProduct();
+        }
+        ValidationResult ValidateProduct(Product product)
+        {
+            return new LiteValidator<Product>()
+            .AddRule(f => !string.IsNullOrEmpty(f.Name))
+            .AddError("*** Nome não pode ser nulo. ***")
+            .Then()
+            .AddRule(f => f.Quantity > 0)
+            .AddError("*** Quantidade deve ser maior que zero. ***")
+            .Then()
+            .AddRule(f => f.Price > 0)
+            .AddError("*** Preço deve ser maior que zero. ***")
+            .Validate(product);
+        }
+
+        public void SaveProduct()
+        {
+            //Implementação de persistência
+            Console.WriteLine("Salvando Product.");
+        }
+    }
+}
