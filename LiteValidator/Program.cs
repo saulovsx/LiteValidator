16758f7ec5691caad0f930195fe580cb94a2b910
Saulo Vinícius Silva <saulovsx@gmail.com>
2024-02-12 16:43:04 -0300

    feat: implementação

--- LiteValidator/Program.cs
+++ LiteValidator/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LiteValidator;
+
+Console.WriteLine("Iniciando LiteValidator!");
+Console.WriteLine("------------------------------------\b\n");
+
+var client = new Client(1, "Jhon");
+Console.WriteLine("Adicionando Cliente.");
+client.AddClient();
+Console.WriteLine("\b\nFinalizado processamento de Cliente.");
+Console.WriteLine("------------------------------------\b\n");
+
+
+var product1 = new Product(1, "Water", 0, quantity: 0);
+Console.WriteLine("Adicionando Produto.");
+product1.AddProduct();
+Console.WriteLine("\b\nFinalizado processamento de Produto.");
+Console.WriteLine("------------------------------------\b\n");
